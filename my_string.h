14f71746827729476a91e442a490bf90806aa474
markuskb <markus312@t-online.de>
2014-10-17 16:08:10 +0200

    Dateiname gefixt-passt jetzt zu den anderen Programmteilen

--- my_string.h
+++
@@ -1,7 +0,0 @@
-#ifndef _VERGLEICH_H
-#define _VERGLEICH_H
-
-
-int  umwandeln (char buchstabe);
-int size(char *ptr);
-int vergleich (char* vorname, char* nachname, char* vorname1, char* nachname1);
